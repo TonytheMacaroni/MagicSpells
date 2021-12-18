983a64659021c04f2e17fe3e844040e3dab41c80
tonythemacaroni <tonythemacaroni123@gmail.com>
2021-12-17 21:41:46 -0500

    Add function support for RiptideSpell

--- core/src/main/java/com/nisovin/magicspells/spells/targeted/RiptideSpell.java
+++ core/src/main/java/com/nisovin/magicspells/spells/targeted/RiptideSpell.java
@@ -6,17 +6,18 @@ import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.util.TargetInfo;
 import com.nisovin.magicspells.util.MagicConfig;
 import com.nisovin.magicspells.spells.TargetedSpell;
+import com.nisovin.magicspells.util.config.ConfigData;
 import com.nisovin.magicspells.spells.TargetedEntitySpell;
 import com.nisovin.magicspells.spelleffects.EffectPosition;
 
 public class RiptideSpell extends TargetedSpell implements TargetedEntitySpell {
 
-	private int duration;
+	private ConfigData<Integer> duration;
 
 	public RiptideSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 
-		duration = getConfigInt("duration", 40);
+		duration = getConfigDataInt("duration", 40);
 	}
 
 	@Override
@@ -27,30 +28,36 @@ public class RiptideSpell extends TargetedSpell implements TargetedEntitySpell {
 		if (target == null) return noTarget(caster);
 		playSpellEffects(caster, target.getTarget());
 
-		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target.getTarget(), duration);
+		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target.getTarget(), duration.get(caster, null, power, args));
 		return PostCastAction.HANDLE_NORMALLY;
 	}
 
 	@Override
-	public boolean castAtEntity(LivingEntity caster, LivingEntity target, float power) {
-		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target, duration);
+	public boolean castAtEntity(LivingEntity caster, LivingEntity target, float power, String[] args) {
+		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target, duration.get(caster, target, power, args));
 		playSpellEffects(caster, target);
 		return true;
 	}
 
 	@Override
-	public boolean castAtEntity(LivingEntity target, float power) {
-		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target, duration);
+	public boolean castAtEntity(LivingEntity caster, LivingEntity target, float power) {
+		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target, duration.get(caster, target, power, null));
+		playSpellEffects(caster, target);
+		return true;
+	}
+
+	@Override
+	public boolean castAtEntity(LivingEntity target, float power, String[] args) {
+		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target, duration.get(null, target, power, args));
 		playSpellEffects(EffectPosition.TARGET, target);
 		return true;
 	}
 
-	public int getDuration() {
-		return duration;
-	}
-
-	public void setDuration(int duration) {
-		this.duration = duration;
+	@Override
+	public boolean castAtEntity(LivingEntity target, float power) {
+		MagicSpells.getVolatileCodeHandler().startAutoSpinAttack(target, duration.get(null, target, power, null));
+		playSpellEffects(EffectPosition.TARGET, target);
+		return true;
 	}
 
 }
