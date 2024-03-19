f7e9fdd2ac04644cabbb4a3d5f9ed538f78ae7db
TonytheMacaroni <tonythemacaroni123@gmail.com>
2024-03-18 20:49:10 -0400

    Refactor magic items, expand debug

--- core/src/main/java/com/nisovin/magicspells/util/itemreader/FireworkHandler.java
+++ core/src/main/java/com/nisovin/magicspells/util/itemreader/FireworkHandler.java
@@ -1,91 +1,106 @@
 package com.nisovin.magicspells.util.itemreader;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 import java.util.ArrayList;
 
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.configuration.ConfigurationSection;
 
 import com.nisovin.magicspells.debug.MagicDebug;
-import com.nisovin.magicspells.util.Util;
-import com.nisovin.magicspells.handlers.DebugHandler;
 import com.nisovin.magicspells.util.magicitems.MagicItemData;
-import static com.nisovin.magicspells.util.magicitems.MagicItemData.MagicItemAttribute.POWER;
-import static com.nisovin.magicspells.util.magicitems.MagicItemData.MagicItemAttribute.FIREWORK_EFFECTS;
 
-public class FireworkHandler {
+import static com.nisovin.magicspells.util.magicitems.MagicItemData.MagicItemAttributes.POWER;
+import static com.nisovin.magicspells.util.magicitems.MagicItemData.MagicItemAttributes.FIREWORK_EFFECTS;
 
-	private static final String FIREWORK_EFFECTS_CONFIG_NAME = FIREWORK_EFFECTS.toString();
-	private static final String POWER_CONFIG_NAME = POWER.toString();
+public class FireworkHandler extends ItemHandler {
 
-	public static void process(ConfigurationSection config, ItemMeta meta, MagicItemData data) {
-		if (!(meta instanceof FireworkMeta fireworkMeta)) return;
+	@Override
+	public boolean process(@NotNull ConfigurationSection config, @NotNull ItemStack item, @NotNull ItemMeta meta, @NotNull MagicItemData data) {
+		if (!(meta instanceof FireworkMeta fireworkMeta)) return true;
 
-		int power = config.getInt(POWER_CONFIG_NAME, 0);
+		if (!config.isInt(POWER.getKey()) && !invalidIfSet(config, POWER))
+			return false;
+
+		int power = config.getInt(POWER.getKey(), 0);
 		fireworkMeta.setPower(power);
 		data.setAttribute(POWER, power);
 
-		if (config.isList(FIREWORK_EFFECTS_CONFIG_NAME)) {
-			List<String> effectStrings = config.getStringList(FIREWORK_EFFECTS_CONFIG_NAME);
+		if (!config.isList(FIREWORK_EFFECTS.getKey())) return invalidIfSet(config, FIREWORK_EFFECTS);
 
-			List<FireworkEffect> fireworkEffects = new ArrayList<>();
+		List<FireworkEffect> fireworkEffects = new ArrayList<>();
+		fireworkMeta.clearEffects();
 
-			// <type> <trail> <flicker> <colors>(,) <fadeColors>(,)
-			for (String effectString : effectStrings) {
-				String[] values = effectString.split(" ");
-				if (values.length != 4 && values.length != 5) {
-					MagicDebug.warn("Invalid firework effect '%s' on magic item - missing or too many values.", effectString);
-					continue;
-				}
-
-				FireworkEffect.Type fireworkType;
-				try {
-					fireworkType = FireworkEffect.Type.valueOf(values[0].toUpperCase());
-				} catch (IllegalArgumentException e) {
-					MagicDebug.warn("Invalid firework effect type '%s' on magic item.", values[0]);
-					continue;
-				}
-				boolean trail = Boolean.parseBoolean(values[1]);
-				boolean flicker = Boolean.parseBoolean(values[2]);
-				List<Color> colors = FireworkEffectHandler.getColorsFromString(values[3]);
-				List<Color> fadeColors = values.length > 4 ? FireworkEffectHandler.getColorsFromString(values[4]) : List.of();
-
-				if (colors.isEmpty()) continue;
-
-				FireworkEffect effect = FireworkEffect.builder()
-						.flicker(flicker)
-						.trail(trail)
-						.with(fireworkType)
-						.withColor(colors)
-						.withFade(fadeColors)
-						.build();
-
-				fireworkEffects.add(effect);
+		// <type> <trail> <flicker> <colors>(,) <fadeColors>(,)
+		List<String> effectStrings = config.getStringList(FIREWORK_EFFECTS.getKey());
+		for (String effectString : effectStrings) {
+			String[] values = effectString.split(" ");
+			if (values.length != 4 && values.length != 5) {
+				MagicDebug.warn("Invalid firework effect '%s' %s - missing or too many values.", effectString, MagicDebug.resolvePath());
+				return false;
 			}
 
-			if (!fireworkEffects.isEmpty()) {
-				fireworkMeta.addEffects(fireworkEffects);
-				data.setAttribute(FIREWORK_EFFECTS, fireworkEffects);
+			FireworkEffect.Type type;
+			try {
+				type = FireworkEffect.Type.valueOf(values[0].toUpperCase());
+			} catch (IllegalArgumentException e) {
+				MagicDebug.warn("Invalid firework effect type '%s' %s.", values[0], MagicDebug.resolvePath());
+				return false;
 			}
+
+			boolean trail = Boolean.parseBoolean(values[1]);
+			boolean flicker = Boolean.parseBoolean(values[2]);
+
+			List<Color> colors = FireworkEffectHandler.getColorsFromString(values[3], "colors", true);
+			if (colors == null) return false;
+
+			List<Color> fadeColors = values.length > 4 ? FireworkEffectHandler.getColorsFromString(values[4], "fade colors", true) : List.of();
+			if (fadeColors == null) return false;
+
+			FireworkEffect effect = FireworkEffect.builder()
+				.flicker(flicker)
+				.trail(trail)
+				.with(type)
+				.withColor(colors)
+				.withFade(fadeColors)
+				.build();
+
+			fireworkEffects.add(effect);
+		}
+
+		if (!fireworkEffects.isEmpty()) {
+			fireworkMeta.addEffects(fireworkEffects);
+			data.setAttribute(FIREWORK_EFFECTS, fireworkEffects);
+		}
+
+		return true;
+	}
+
+	@Override
+	public void processItemMeta(@NotNull ItemStack item, @NotNull ItemMeta meta, @NotNull MagicItemData data) {
+		if (!(meta instanceof FireworkMeta fireworkMeta)) return;
+
+		if (data.hasAttribute(POWER)) fireworkMeta.setPower(data.getAttribute(POWER));
+
+		if (data.hasAttribute(FIREWORK_EFFECTS)) {
+			fireworkMeta.clearEffects();
+			fireworkMeta.addEffects(data.getAttribute(FIREWORK_EFFECTS));
 		}
 	}
 
-	public static void processItemMeta(ItemMeta meta, MagicItemData data) {
+	@Override
+	public void processMagicItemData(@NotNull ItemStack item, @NotNull ItemMeta meta, @NotNull MagicItemData data) {
 		if (!(meta instanceof FireworkMeta fireworkMeta)) return;
 
-		if (data.hasAttribute(POWER)) fireworkMeta.setPower((int) data.getAttribute(POWER));
-		if (data.hasAttribute(FIREWORK_EFFECTS)) fireworkMeta.addEffects((List<FireworkEffect>) data.getAttribute(FIREWORK_EFFECTS));
-	}
-
-	public static void processMagicItemData(ItemMeta meta, MagicItemData data) {
-		if (!(meta instanceof FireworkMeta fireworkMeta)) return;
 		data.setAttribute(POWER, fireworkMeta.getPower());
-		if (!fireworkMeta.hasEffects()) return;
+
 		List<FireworkEffect> effects = fireworkMeta.getEffects();
-		if (!effects.isEmpty()) data.setAttribute(FIREWORK_EFFECTS, fireworkMeta.getEffects());
+		if (!effects.isEmpty()) data.setAttribute(FIREWORK_EFFECTS, effects);
 	}
 
 }
--- core/src/main/java/com/nisovin/magicspells/util/itemreader/RepairableHandler.java
+++ core/src/main/java/com/nisovin/magicspells/util/itemreader/RepairableHandler.java
@@ -1,34 +1,42 @@
 package com.nisovin.magicspells.util.itemreader;
 
+import org.jetbrains.annotations.NotNull;
+
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.Repairable;
 import org.bukkit.configuration.ConfigurationSection;
 
 import com.nisovin.magicspells.util.magicitems.MagicItemData;
-import static com.nisovin.magicspells.util.magicitems.MagicItemData.MagicItemAttribute.REPAIR_COST;
 
-public class RepairableHandler {
+import static com.nisovin.magicspells.util.magicitems.MagicItemData.MagicItemAttributes.REPAIR_COST;
 
-	private static final String CONFIG_NAME = REPAIR_COST.toString();
+public class RepairableHandler extends ItemHandler {
 
-	public static void process(ConfigurationSection config, ItemMeta meta, MagicItemData data) {
-		if (!(meta instanceof Repairable)) return;
-		if (!config.isInt(CONFIG_NAME)) return;
+	@Override
+	public boolean process(@NotNull ConfigurationSection config, @NotNull ItemStack item, @NotNull ItemMeta meta, @NotNull MagicItemData data) {
+		if (!(meta instanceof Repairable repairable)) return true;
 
-		int repairCost = config.getInt(CONFIG_NAME);
-		((Repairable) meta).setRepairCost(repairCost);
+		if (!config.isInt(REPAIR_COST.getKey())) return invalidIfSet(config, REPAIR_COST);
+
+		int repairCost = config.getInt(REPAIR_COST.getKey());
+		repairable.setRepairCost(repairCost);
 		data.setAttribute(REPAIR_COST, repairCost);
+
+		return true;
 	}
 
-	public static void processItemMeta(ItemMeta meta, MagicItemData data) {
-		if (!(meta instanceof Repairable repairable)) return;
-		if (!data.hasAttribute(REPAIR_COST)) return;
-		repairable.setRepairCost((int) data.getAttribute(REPAIR_COST));
+	@Override
+	public void processItemMeta(@NotNull ItemStack item, @NotNull ItemMeta meta, @NotNull MagicItemData data) {
+		if (!(meta instanceof Repairable repairable) || !data.hasAttribute(REPAIR_COST)) return;
+
+		repairable.setRepairCost(data.getAttribute(REPAIR_COST));
 	}
 
-	public static void processMagicItemData(ItemMeta meta, MagicItemData data) {
-		if (!(meta instanceof Repairable repairable)) return;
-		if (!repairable.hasRepairCost()) return;
+	@Override
+	public void processMagicItemData(@NotNull ItemStack item, @NotNull ItemMeta meta, @NotNull MagicItemData data) {
+		if (!(meta instanceof Repairable repairable) || !repairable.hasRepairCost()) return;
+
 		data.setAttribute(REPAIR_COST, repairable.getRepairCost());
 	}
 	
