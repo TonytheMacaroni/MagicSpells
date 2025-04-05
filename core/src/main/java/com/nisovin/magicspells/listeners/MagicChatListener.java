525b2d72406d1272f04f432b102bb47959470741
TonytheMacaroni <tonythemacaroni123@gmail.com>
2025-04-05 12:20:05 -0400

    Cleanup, allow using incantations for helper spells

--- core/src/main/java/com/nisovin/magicspells/listeners/MagicChatListener.java
+++ core/src/main/java/com/nisovin/magicspells/listeners/MagicChatListener.java
@@ -1,7 +1,6 @@
 package com.nisovin.magicspells.listeners;
 
 import com.nisovin.magicspells.Spell;
-import com.nisovin.magicspells.Spellbook;
 import com.nisovin.magicspells.util.Util;
 import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.util.SpellData;
@@ -14,42 +13,42 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class MagicChatListener implements Listener {
-	
+
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerChat(final AsyncChatEvent event) {
 		handleIncantation(event.getPlayer(), Util.getStringFromComponent(event.message()));
 	}
-	
+
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
 		boolean casted = handleIncantation(event.getPlayer(), event.getMessage());
 		if (casted) event.setCancelled(true);
 	}
-	
+
 	private boolean handleIncantation(Player player, String message) {
+		argument_check:
 		if (message.contains(" ")) {
 			String[] split = message.split(" ");
+
 			Spell spell = MagicSpells.getIncantations().get(split[0].toLowerCase() + " *");
-			if (spell != null) {
-				Spellbook spellbook = MagicSpells.getSpellbook(player);
-				if (spellbook.hasSpell(spell)) {
-					String[] args = new String[split.length - 1];
-					System.arraycopy(split, 1, args, 0, args.length);
-					MagicSpells.scheduleDelayedTask(() -> spell.hardCast(new SpellData(player, 1f, args)), 0);
-					return true;
-				}
+			if (spell == null) break argument_check;
+
+			if (!spell.isHelperSpell() && !MagicSpells.getSpellbook(player).hasSpell(spell))
 				return false;
-			}
+
+			String[] args = new String[split.length - 1];
+			System.arraycopy(split, 1, args, 0, args.length);
+
+			MagicSpells.scheduleDelayedTask(() -> spell.hardCast(new SpellData(player, 1f, args)), 0);
+			return true;
 		}
+
 		Spell spell = MagicSpells.getIncantations().get(message.toLowerCase());
-		if (spell != null) {
-			Spellbook spellbook = MagicSpells.getSpellbook(player);
-			if (spellbook.hasSpell(spell)) {
-				MagicSpells.scheduleDelayedTask(() -> spell.hardCast(new SpellData(player)), 0);
-				return true;
-			}
-		}
-		return false;
+		if (spell == null || !spell.isHelperSpell() && !MagicSpells.getSpellbook(player).hasSpell(spell))
+			return false;
+
+		MagicSpells.scheduleDelayedTask(() -> spell.hardCast(new SpellData(player)), 0);
+		return true;
 	}
-	
+
 }
